154809026542ed125e89fcf7d6d1b5487fa5723e
shift02 <shift02ss@gmail.com>
2016-10-05 23:30:13 +0900

    追加：コイン

---
+++ src/main/java/shift/mceconomy3compat/ItemCoin.java
@@ -0,0 +1,64 @@
+package shift.mceconomy3compat;
+
+import java.util.List;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import shift.mceconomy3.api.MCEconomyAPI;
+
+public class ItemCoin extends Item {
+
+    public ItemCoin() {
+        this.setCreativeTab(CreativeTabs.MISC);
+        //this.setMaxDamage(Integer.MAX_VALUE);
+        this.setHasSubtypes(true);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+        subItems.add(new ItemStack(itemIn, 1, 1));
+        subItems.add(new ItemStack(itemIn, 1, 5));
+        subItems.add(new ItemStack(itemIn, 1, 10));
+        subItems.add(new ItemStack(itemIn, 1, 50));
+        subItems.add(new ItemStack(itemIn, 1, 100));
+        subItems.add(new ItemStack(itemIn, 1, 500));
+        subItems.add(new ItemStack(itemIn, 1, 1000));
+        subItems.add(new ItemStack(itemIn, 1, 5000));
+        subItems.add(new ItemStack(itemIn, 1, 10000));
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn,
+            EnumHand hand) {
+        if (!playerIn.capabilities.isCreativeMode) {
+            --itemStackIn.stackSize;
+        }
+
+        if (!worldIn.isRemote) {
+
+            MCEconomyAPI.addPlayerMP(playerIn, itemStackIn.getItemDamage(), false);
+
+            System.out.println(MCEconomy3Compat.shopID);
+            MCEconomyAPI.openShopGui(MCEconomy3Compat.shopID, playerIn, worldIn, (int) playerIn.posX,
+                    (int) playerIn.posY, (int) playerIn.posZ);
+
+        }
+
+        return new ActionResult(EnumActionResult.SUCCESS, itemStackIn);
+    }
+
+    @Override
+    public int getMetadata(ItemStack stack) {
+        return 0;
+    }
+
+}
